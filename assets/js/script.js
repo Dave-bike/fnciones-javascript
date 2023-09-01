0684d843dc1cdc0de63cd3f2988363fa2367b46b
dave-bike <ceaconstruye@gmail.com>
2023-08-31 23:05:15 -0400

    avance

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,14 @@ ele.addEventListener("click", pintar);*/
 
 /*ejercicio 3.2*/
 
-const pintar = (item) => (item.style.backgroundColor = "yellow");
+/*function pintar(item) {
+  return (item.style.backgroundColor = "yellow");
+}
 document
   .getElementById("ele1")
-  .addEventListener("click", ({ target }) => pintar(target));
+  .addEventListener("click", ({ target }) => pintar(target)); */
+
+const pintar = (item, color = 'green') => item.style.backgroundColor = color
+document
+  .getElementById("ele1")
+  .addEventListener("click", ({ target }) => pintar(target, "yellow"))
